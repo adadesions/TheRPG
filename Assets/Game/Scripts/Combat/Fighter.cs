d05bf103ecbf4c2ac39ffff1c809735d145a6ce4
adadesions <adadesions@gmail.com>
2022-02-21 20:10:47 +0700

    4th commit

--- Assets/Game/Scripts/Combat/Fighter.cs
+++ Assets/Game/Scripts/Combat/Fighter.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TheRPG.Movement;
 using UnityEngine;
 
 namespace TheRPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
-        public void Attack()
+        Transform target;
+
+        void Update()
         {
+            if (target != null)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+                target = null;
+            }    
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
             print("Attacking!!");
         }
     }
