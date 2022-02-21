9bde4a61734a084be48d388018750a85db6935b1
adadesions <adadesions@gmail.com>
2022-02-21 19:38:30 +0700

    3rd commit

--- Assets/Game/Scripts/Controller/PlayerController.cs
+++ Assets/Game/Scripts/Controller/PlayerController.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TheRPG.Movement;
+using System;
+using TheRPG.Combat;
 
 namespace TheRPG.Controller
 {
     public class PlayerController : MonoBehaviour
     {
         void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack();
+                }
+            }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
