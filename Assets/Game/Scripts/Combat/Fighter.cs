cf2ae1b85bac175eeafa15c95b09c0d2d4f89885
adadesions <adadesions@gmail.com>
2022-02-21 18:16:12 +0700

    Auto point to enermy

--- Assets/Game/Scripts/Combat/Fighter.cs
+++ Assets/Game/Scripts/Combat/Fighter.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TheRPG.Movement;
 using UnityEngine;
 
 namespace TheRPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
-        public void Attack(CombatTarget target)
+        //[SerializeField] float weaponRange = 2f;
+        Transform target;
+
+        public void Update()
         {
+            //bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
+            if (target != null)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+                
+            }
+            else
+            {
+                GetComponent<Mover>().Stop();
+            }
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
             print("Attacking: " + target.name);
         }
     }
