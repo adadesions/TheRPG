562823a86004ed83cdcbb408dc6180e685340572
adadesions <adadesions@gmail.com>
2022-04-08 20:02:03 +0700

    Finish EP8

--- Assets/Game/Scripts/Controller/EnemyController.cs
+++ Assets/Game/Scripts/Controller/EnemyController.cs
@@ -2,24 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TheRPG.Combat;
 
 namespace TheRPG.Controller
 {
     public class EnemyController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 6f;
+        Fighter fighter;
+        GameObject player;
+
+        private void Start()
+        {
+            fighter = GetComponent<Fighter>();
+            player = GameObject.FindWithTag("Player");
+
+        }
 
         private void Update()
         {
             if (DistanceToPlayer() < chaseDistance)
             {
-                print("Should chase!!");
+                fighter.Attack(player);
+            }
+            else
+            {
+                fighter.Cancel();
             }
         }
 
         private float DistanceToPlayer()
         {
-            GameObject player = GameObject.FindWithTag("Player");
             Vector3 playerPosition = player.transform.position;
             return Vector3.Distance(transform.position, playerPosition);
         }
