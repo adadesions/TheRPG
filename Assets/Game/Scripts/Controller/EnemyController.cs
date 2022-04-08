036e70ae98f510bf10c9addd296f46c965828e9d
adadesions <adadesions@gmail.com>
2022-04-08 18:37:16 +0700

    Add EnemyController

---
+++ Assets/Game/Scripts/Controller/EnemyController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheRPG.Controller
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 6f;
+
+        private void Update()
+        {
+            if (DistanceToPlayer() < chaseDistance)
+            {
+                print("Should chase!!");
+            }
+        }
+
+        private float DistanceToPlayer()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            Vector3 playerPosition = player.transform.position;
+            return Vector3.Distance(transform.position, playerPosition);
+        }
+    }
+}
+
