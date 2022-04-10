a9a3b56ef811cf94d583d47e110da48e9227057d
adadesions <adadesions@gmail.com>
2022-04-10 14:23:36 +0700

    Draw loop

--- Assets/Game/Scripts/Controller/WayPoints.cs
+++ Assets/Game/Scripts/Controller/WayPoints.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WayPoints : MonoBehaviour
+namespace TheRPG.Controller
 {
-    // Start is called before the first frame update
-    void Start()
+    public class WayPoints : MonoBehaviour
     {
-        
-    }
+        const float radius = 0.5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int nextIdx = GetNextIndex(i);
+                Gizmos.DrawSphere(GetPoint(i), radius);
+                Gizmos.DrawLine(GetPoint(i), GetPoint(nextIdx));
+            }
+        }
+
+        private int GetNextIndex(int idx)
+        {
+            if (idx+1 == transform.childCount)
+            {
+                return 0;
+            }
+
+            return idx + 1;
+        }
+
+        private Vector3 GetPoint(int idx)
+        {
+            return transform.GetChild(idx).position;
+        }
     }
 }
+
