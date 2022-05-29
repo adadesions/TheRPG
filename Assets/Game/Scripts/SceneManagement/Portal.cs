9724456582e0a744aa3baacc93df98717e0c24a1
adadesions <adadesions@gmail.com>
2022-05-29 13:33:40 +0700

    Start Portal

---
+++ Assets/Game/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheRPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
+
