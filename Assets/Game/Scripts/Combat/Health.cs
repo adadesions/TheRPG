424b04994c5952e49fe6dcc47a36a8d4274fb268
adadesions <adadesions@gmail.com>
2022-07-04 18:35:11 +0700

    Add Healthbars

--- Assets/Game/Scripts/Combat/Health.cs
+++ Assets/Game/Scripts/Combat/Health.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TheRPG.Core;
 
 namespace TheRPG.Combat
-{
+{    
     public class Health : MonoBehaviour
     {
         [SerializeField] float HP = 100f;
+        [SerializeField] float maxHP = 100f;
+        [SerializeField] Image globalHPBar;
+        [SerializeField] GameObject localHPBar;
 
         private bool isDead = false;
+
+        private void UpdateHPBar() {
+            // Clamp(calValue, minRange, maxRange)
+            float hpValue = Mathf.Clamp(HP / maxHP, 0f, 1f);
+            if (globalHPBar) {
+                globalHPBar.fillAmount = hpValue;
+            }
+
+            if (localHPBar) {
+                Vector3 curBar = localHPBar.transform.localScale;
+                localHPBar.transform.localScale = new Vector3(hpValue, curBar.y, curBar.z);
+            }            
+        }
 
         public void TakeDamage(float damage)
         {
             HP = Mathf.Max(HP - damage, 0);
             print(name + "'s Health: " + HP);
+            UpdateHPBar();
 
             if (HP <= 0 && !isDead)
             {
